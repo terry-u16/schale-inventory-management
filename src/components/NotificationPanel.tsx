a01155900e3587e584b1c4280f60d45b1b86c86b
terry-u16 <terry-u16@users.noreply.github.com>
2024-10-08 22:21:47 +0900

    2024/10/10開始総決算に向けて注意書きを更新

--- src/components/NotificationPanel.tsx
+++ src/components/NotificationPanel.tsx
@@ -6,9 +6,8 @@ const NotificationPanel: FC = () => {
     <>
       <Paper>
         <Box p={2} textAlign="start">
-          <Alert severity="success">
-            2024/9/25開始の山海経イベントへの対応を完了しました。
-            お気付きの点がございましたら
+          <Alert severity="warning">
+            現在、2024/9/25開始の山海経イベントへの対応は完了しておりますが、2024/10/10開始の総決算イベントには未対応です。新イベントの各周回におけるアイテムの大きさ・数量をご存じの方は、
             <a
               href="https://github.com/terry-u16/schale-inventory-management/issues"
               target="_blank"
@@ -17,6 +16,7 @@ const NotificationPanel: FC = () => {
               githubのissue
             </a>
             にてご報告頂けますと幸いです。
+            ご不便をおかけしますが、どうぞよろしくお願いいたします。
           </Alert>
         </Box>
       </Paper>
