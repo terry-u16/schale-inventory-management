dce99f35de4c2f37df2c7521e36cb777cff643b2
puttimeth <puttimeth.suppawawisit@gmail.com>
2025-03-20 22:51:52 +0700

    Add localization package and language dropdown

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './i18n.ts';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
